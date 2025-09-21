888d6f23e43e37914c7bfc9d27249c8b5248a761
Piotr Mulinski <piotr.mulinski@icloud.com>
2025-09-21 15:16:13 +0000

    Slotmachine met map klaar

--- labo1-slotmachine-map/src/App.tsx
+++ labo1-slotmachine-map/src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import {cherry, lemon, watermelon, plum, seven} from "./assets";
+
+const AMOUNT_SLOTS = 3;
+const images = [cherry, lemon, watermelon, plum, seven];
+
+const fillArray = (numberOfSlots: number) => {
+  const arr: number[] = [];
+
+  for (let i = 0; i < numberOfSlots; i++) {
+    arr.push(Math.floor(Math.random() * 5));
+  }
+
+  return arr;
+};
+
+const resetPage = () => {
+  window.location.reload();
+  return;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const slots = fillArray(AMOUNT_SLOTS);
+  const winning = slots.every((item) => item === slots[0]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className="left-algin">Labo 1: Slots</h1>
+
+      <div className="container_slot">
+        {images.map((src, i) => (
+          <img key={i} className="picture_box" src={src} alt={`icon ${i}`} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="msg_box">
+        <h2>{winning ? "Jij hebt gewonnen" : "Jij hebt verloren !"}</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="msg_box container_slot">
+        {slots.map((slot, i) => (
+          <img
+            key={i}
+            className="picture_box"
+            src={images[slot]}
+            alt={images[slot] + " picture"}
+          />
+        ))}
+      </div>
+
+      <button type="reset" onClick={resetPage}>
+        Reset
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
