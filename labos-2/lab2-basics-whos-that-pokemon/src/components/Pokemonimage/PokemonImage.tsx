7e2182b77641e8e4ab657594c3742bbdc2722b19
Piotr Mulinski <piotr.mulinski@icloud.com>
2025-11-16 21:50:22 +0000

    lab2-basics-whos-that-pokemon klaar

---
+++ labos-2/lab2-basics-whos-that-pokemon/src/components/Pokemonimage/PokemonImage.tsx
@@ -0,0 +1,25 @@
+interface PokemonImageProps {
+  id: number;
+  visible: boolean;
+  size: string;
+}
+
+const PokemonImage = ({id, visible, size = "16"}: PokemonImageProps) => {
+  const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+
+  return (
+    <>
+      <img
+        style={{
+          ...(visible ? {} : {filter: "brightness(0)"}),
+          //   filter: visible ? "none" : "brightness(0)",
+          width: `${size}rem`,
+        }}
+        src={imgUrl}
+        alt="pokemon"
+      />
+    </>
+  );
+};
+
+export default PokemonImage;
