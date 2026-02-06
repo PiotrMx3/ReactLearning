17e5a50dadd636d8605e1b011b7472420d9c77a1
Piotr Mulinski <piotr.mulinski@icloud.com>
2026-02-06 20:50:02 +0000

    labo7-communication-counter-list klaar

---
+++ labos-7/labo7-communication-counter-list/src/components/Counter/Counter.tsx
@@ -0,0 +1,42 @@
+interface CounterProps {
+  value: number;
+  onIncrease: (index: number) => void;
+  onDecrease: (index: number) => void;
+  index: number;
+}
+
+const Counter = ({value, onIncrease, onDecrease, index}: CounterProps) => {
+  let color = "black";
+  if (value > 0) {
+    color = "green";
+  } else if (value < 0) {
+    color = "red";
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <button onClick={() => onDecrease(index)}>Omlaag</button>
+      <div
+        style={{
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: color,
+        }}
+      >
+        Count: {value}
+      </div>
+      <button onClick={() => onIncrease(index)}>Omhoog</button>
+    </div>
+  );
+};
+
+export default Counter;
